cdbc4f6f6e3799e11b3e28caa47b4982c416388d
Robert Winkler <robert.winkler@telekom.de>
2015-05-06 12:43:40 +0200

    Added Swagger2Markup Gradle Plugin

--- src/test/java/io/github/robwin/swagger2markup/petstore/Swagger2MarkupTest.java
+++ src/test/java/io/github/robwin/swagger2markup/petstore/Swagger2MarkupTest.java
@@ -5,10 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.robwin.swagger2markup.petstore.model.Category;
 import io.github.robwin.swagger2markup.petstore.model.Pet;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,14 +20,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import springfox.documentation.staticdocs.SwaggerResultHandler;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Swagger2MarkupTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(Swagger2MarkupTest.class);
@@ -53,21 +52,14 @@ public class Swagger2MarkupTest {
                 .andExpect(status().isOk());
     }
 
-    /*
-
     @Test
-    public void zConvertSwaggerToAsciiDoc() throws Exception {
+    public void createSwaggerJson() throws Exception {
         String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
-        String examplesOutputDir = System.getProperty("org.springframework.restdocs.outputDir");
-
         this.mockMvc.perform(get("/v2/api-docs")
                 .accept(MediaType.APPLICATION_JSON))
-                .andDo(document("get_swagger_doc"))
-                .andDo(Swagger2MarkupResultHandler.outputDirectory(outputDir)
-                        .withExamples(examplesOutputDir).build())
+                .andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
                 .andExpect(status().isOk());
     }
-    */
 
     private String createPet() throws JsonProcessingException {
         Pet pet = new Pet();
