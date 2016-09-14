30aaf8519789f8eefaf114d74e1525f214f30829
Robert Winkler <rwinkler@telekom.de>
2016-09-14 09:54:46 +0200

    Updated to Spring Boot 1.4.0.RELEASE

--- src/test/java/io/github/robwin/swagger2markup/petstore/Swagger2MarkupTest.java
+++ src/test/java/io/github/robwin/swagger2markup/petstore/Swagger2MarkupTest.java
@@ -23,26 +23,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.robwin.swagger2markup.SwaggerConfig;
 import io.github.robwin.swagger2markup.petstore.model.Category;
 import io.github.robwin.swagger2markup.petstore.model.Pet;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentation;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import springfox.documentation.staticdocs.SwaggerResultHandler;
+
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -51,25 +52,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {Application.class, SwaggerConfig.class})
+@AutoConfigureRestDocs(outputDir = "build/asciidoc/snippets")
+@SpringBootTest(classes = {Application.class, SwaggerConfig.class})
+@AutoConfigureMockMvc
 public class Swagger2MarkupTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(Swagger2MarkupTest.class);
 
+
     @Autowired
-    private WebApplicationContext context;
-
     private MockMvc mockMvc;
-
-    @Rule
-    public final RestDocumentation restDocumentation = new RestDocumentation(System.getProperty("io.springfox.staticdocs.snippetsOutputDir"));
-
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-                .apply(documentationConfiguration(this.restDocumentation))
-                .build();
-    }
 
     @Test
     public void addANewPetToTheStore() throws Exception {
@@ -86,12 +78,15 @@ public class Swagger2MarkupTest {
         String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
         MvcResult mvcResult = this.mockMvc.perform(get("/v2/api-docs")
                 .accept(MediaType.APPLICATION_JSON))
-                .andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
                 .andExpect(status().isOk())
                 .andReturn();
 
-        //String springfoxSwaggerJson = mvcResult.getResponse().getContentAsString();
-        //SwaggerAssertions.assertThat(Swagger20Parser.parse(springfoxSwaggerJson)).isEqualTo(designFirstSwaggerLocation);
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String swaggerJson = response.getContentAsString();
+        Files.createDirectories(Paths.get(outputDir));
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputDir, "swagger.json"), StandardCharsets.UTF_8)){
+            writer.write(swaggerJson);
+        }
     }
 
     private String createPet() throws JsonProcessingException {
